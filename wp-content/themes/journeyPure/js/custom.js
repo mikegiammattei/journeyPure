2227ef4d658c7bc88169964be0cf259de65b7921
mikegiammattei <michaelgiammattei@gmail.com>
2019-12-13 09:33:41 -0500

    Updated core js files.

--- wp-content/themes/journeyPure/js/custom.js
+++ wp-content/themes/journeyPure/js/custom.js
@@ -460,6 +460,13 @@ function checkInsurance() {
 cta_widget();
 
 function cta_widget() {
+  // CTM implementation code block
+  var ctm_frame = $("iframe[src*='https://130400.tctm.co/form/FRT472ABB2C5B9B141A95E7A133293232FBB3EFEC59E103154BC3C3A194C8DE5FD3.html']").get(0);
+  $(ctm_frame).on('load', function () {
+    ctm_frame.contentWindow.postMessage(window.JSON.stringify({
+      action: 'showForm'
+    }), ctm_frame.src);
+  });
   var parentClass = '.cta-widget';
   var cta = {
     el: $(parentClass),
