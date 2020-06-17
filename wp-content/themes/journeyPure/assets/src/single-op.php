bd7c32e90882e5d9bcb4a2093512b559b193ce3e
Fernando Tessmann <nandotess85@gmail.com>
2020-06-16 23:26:01 -0400

    New outpatient template (on going)

--- wp-content/themes/journeyPure/assets/src/single-op.php
+++ wp-content/themes/journeyPure/assets/src/single-op.php
@@ -22,8 +22,8 @@ get_header();
 		<div class="container">
 			<div class="row">
 				<div class="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
-					<h1 class="jp-op-masthead-h1"><?php echo esc_html( $op->masthead_title ); ?></h1>
-					<h2 class="jp-op-masthead-h2"><?php echo esc_html( $op->masthead_subtitle ); ?></h2>
+					<h1 class="jp-op-masthead-title"><?php echo esc_html( $op->masthead_title ); ?></h1>
+					<h2 class="jp-op-masthead-subtitle"><?php echo esc_html( $op->masthead_subtitle ); ?></h2>
 				</div>
 			</div>
 
@@ -42,8 +42,8 @@ get_header();
 							<p>Whether it's here or not, make the call and show up. Everything will start to come together after that. You can do this!</p>
 						</div>
 
-						<a class="jp-op-masthead-button hide-for-sm" href="tel:844-505-4799" title="Call (844) 505-4799 to talk now."><i class="fas fa-phone"></i> (844) 505-4799</a>
-						<a class="jp-op-masthead-button show-for-sm" href="javascript:void(0);" data-toggle="tooltip" data-placement="top" data-trigger="click" title="Call (844) 505-4799 to talk now."><i class="fas fa-phone"></i> (844) 505-4799</a>
+						<a class="jp-op-masthead-button hide-for-sm" href="tel:844-505-4799" title="Call (844) 505-4799 to talk now."><span class="fas fa-phone"></span> (844) 505-4799</a>
+						<a class="jp-op-masthead-button show-for-sm" href="javascript:void(0);" data-toggle="tooltip" data-placement="top" data-trigger="click" title="Call (844) 505-4799 to talk now."><span class="fas fa-phone"></span> (844) 505-4799</a>
 					</div>
 				</div>
 			</div>
@@ -58,36 +58,36 @@ get_header();
 		<div class="container">
 			<div class="row">
 				<div class="col-12 col-md-6 col-lg-7">
-					<h3 class="jp-op-highlights-h3">Clinic Highlights</h3>
+					<h3 class="jp-op-highlights-title">Clinic Highlights</h3>
 
 					<ul class="jp-op-highlights-list">
 						<li class="jp-op-highlights-list-item">
-							<i class="far fa-check-circle jp-op-highlights-list-item-icon"></i>
-							<span class="jp-op-highlights-list-item-heading">95% satisfaction rating</span>
+							<span class="far fa-check-circle jp-op-highlights-list-item-icon"></span>
+							<span class="jp-op-highlights-list-item-title">95% satisfaction rating</span>
 							<span class="jp-op-highlights-list-item-text">And, 6000 success stories</span>
 						</li>
 
 						<li class="jp-op-highlights-list-item">
-							<i class="far fa-check-circle jp-op-highlights-list-item-icon"></i>
-							<span class="jp-op-highlights-list-item-heading">Success rates 2x higher than average</span>
+							<span class="far fa-check-circle jp-op-highlights-list-item-icon"></span>
+							<span class="jp-op-highlights-list-item-title">Success rates 2x higher than average</span>
 							<span class="jp-op-highlights-list-item-text">Compared to data from the National Institute on Drug & Alcohol Abuse</span>
 						</li>
 
 						<li class="jp-op-highlights-list-item">
-							<i class="far fa-check-circle jp-op-highlights-list-item-icon"></i>
-							<span class="jp-op-highlights-list-item-heading">Masters-level+ clinical and medical team</span>
+							<span class="far fa-check-circle jp-op-highlights-list-item-icon"></span>
+							<span class="jp-op-highlights-list-item-title">Masters-level+ clinical and medical team</span>
 							<span class="jp-op-highlights-list-item-text">Who actually care and put you first</span>
 						</li>
 
 						<li class="jp-op-highlights-list-item">
-							<i class="far fa-check-circle jp-op-highlights-list-item-icon"></i>
-							<span class="jp-op-highlights-list-item-heading">CARF Gold Seal</span>
+							<span class="far fa-check-circle jp-op-highlights-list-item-icon"></span>
+							<span class="jp-op-highlights-list-item-title">CARF Gold Seal</span>
 							<span class="jp-op-highlights-list-item-text">Earned only by the top 20% of centers in the country</span>
 						</li>
 
 						<li class="jp-op-highlights-list-item">
-							<i class="far fa-check-circle jp-op-highlights-list-item-icon"></i>
-							<span class="jp-op-highlights-list-item-heading">In-network with almost every insurance</span>
+							<span class="far fa-check-circle jp-op-highlights-list-item-icon"></span>
+							<span class="jp-op-highlights-list-item-title">In-network with almost every insurance</span>
 							<span class="jp-op-highlights-list-item-text">So any cost to you is as low as possible</span>
 						</li>
 					</ul>
@@ -97,7 +97,7 @@ get_header();
 					</div>
 
 					<div class="jp-op-highlights-button-wrapper">
-						<button type="button" data-toggle="modal" data-target="#main-insurance-form" class="jp-op-highlights-button"><i class="fas fa-id-card"></i> Check Insurance</button>
+						<button type="button" data-toggle="modal" data-target="#main-insurance-form" class="jp-op-highlights-button"><span class="fas fa-id-card"></span> Check Insurance</button>
 					</div>
 				</div>
 
@@ -109,6 +109,67 @@ get_header();
 	</section>
 
 	<!-- /SECTION: Clinic Highlights -->
+
+	<!-- SECTION: Table -->
+
+	<section class="jp-op-section jp-op-table">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 col-md-12">
+					<h3 class="jp-op-table-title">Evidenced-Based treatments that attack <span>all</span> the issues that hold you back.</h3>
+					<p class="jp-op-table-subtitle">(including things like depression and anxiety)</p>
+				</div>
+			</div>
+
+			<div class="jp-op-table-cards">
+				<div class="row">
+					<div class="col-12 col-lg-4">
+						<div class="jp-op-table-card">
+							<img data-src="/wp-content/themes/journeyPure/assets/img/op/paper-plane.png" alt="Session Types" class="jp-op-table-card-img lazy">
+							<h4 class="jp-op-table-card-title">Session Types</h4>
+
+							<ul class="jp-op-table-card-list">
+								<li class="jp-op-table-card-list-item">Group Therapy</li>
+								<li class="jp-op-table-card-list-item">Individual Therapy</li>
+								<li class="jp-op-table-card-list-item">Medication Evaluations</li>
+								<li class="jp-op-table-card-list-item">Life Coaching <a href="javascript:void(0);" class="jp-op-table-card-info" data-toggle="tooltip" data-placement="top" data-trigger="trigger" data-html="true" title="Tooltip on top"><span class="fas fa-info-circle"></span></a></li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="col-12 col-lg-4">
+						<div class="jp-op-table-card">
+							<img data-src="/wp-content/themes/journeyPure/assets/img/op/plane.png" alt="Methodologies" class="jp-op-table-card-img lazy">
+							<h4 class="jp-op-table-card-title">Methodologies</h4>
+
+							<ul class="jp-op-table-card-list">
+								<li class="jp-op-table-card-list-item">CBT and DBT Psychotherapy</li>
+								<li class="jp-op-table-card-list-item">Trauma-Informed Care</li>
+								<li class="jp-op-table-card-list-item">Motivational Interviewing</li>
+								<li class="jp-op-table-card-list-item">Relapse Prevention Planning</li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="col-12 col-lg-4">
+						<div class="jp-op-table-card">
+							<img data-src="/wp-content/themes/journeyPure/assets/img/op/space-ship.png" alt="Medications" class="jp-op-table-card-img lazy">
+							<h4 class="jp-op-table-card-title">Medications</h4>
+
+							<ul class="jp-op-table-card-list">
+								<li class="jp-op-table-card-list-item">For everyone <a href="javascript:void(0);" class="jp-op-table-card-info" data-toggle="tooltip" data-placement="top" data-trigger="trigger" data-html="true" title="Tooltip on top"><span class="fas fa-info-circle"></span></a></li>
+								<li class="jp-op-table-card-list-item">For alcohol <a href="javascript:void(0);" class="jp-op-table-card-info" data-toggle="tooltip" data-placement="top" data-trigger="trigger" data-html="true" title="Tooltip on top"><span class="fas fa-info-circle"></span></a></li>
+								<li class="jp-op-table-card-list-item">For opiates <a href="javascript:void(0);" class="jp-op-table-card-info" data-toggle="tooltip" data-placement="top" data-trigger="trigger" data-html="true" title="Tooltip on top"><span class="fas fa-info-circle"></span></a></li>
+								<li class="jp-op-table-card-list-item">For underlying issues <a href="javascript:void(0);" class="jp-op-table-card-info" data-toggle="tooltip" data-placement="top" data-trigger="trigger" data-html="true" title="Tooltip on top"><span class="fas fa-info-circle"></span></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- /SECTION: Table -->
 
 </div>
 
