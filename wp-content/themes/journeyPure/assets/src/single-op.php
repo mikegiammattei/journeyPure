8c811b2e1a19c576cac758e2e007f19484265522
Fernando Tessmann <nandotess85@gmail.com>
2020-06-17 07:44:14 -0400

    New outpatient template (on going)

--- wp-content/themes/journeyPure/assets/src/single-op.php
+++ wp-content/themes/journeyPure/assets/src/single-op.php
@@ -132,7 +132,29 @@ get_header();
 								<li class="jp-op-table-card-list-item">Group Therapy</li>
 								<li class="jp-op-table-card-list-item">Individual Therapy</li>
 								<li class="jp-op-table-card-list-item">Medication Evaluations</li>
-								<li class="jp-op-table-card-list-item">Life Coaching <a href="javascript:void(0);" class="jp-op-table-card-info" data-toggle="tooltip" data-placement="top" data-trigger="trigger" data-html="true" title="Tooltip on top"><span class="fas fa-info-circle"></span></a></li>
+
+								<?php
+									$tooltip = 'Your Life Coach will call you every week in the beginning and for a full year after. You can contact them any time you\'re feeling triggered or need advice - even years later. They\'ve been there before and know what it takes to stay on track.
+									<br><br>
+									The JourneyPure Alumni app is a great way to stay connected.
+									<br><br>
+									The app has:
+									<br>
+									<ul>
+									<li>Direct 24/7 text message connection to your dedicated life coach</li>
+									<li>Personalized reminders to reward you for making healthy choices (with points exchanged for real gift cards)
+									An 11-module self-help course with interactive journals for you to work through at your own pace</li>
+									</ul>';
+								?>
+
+								<li class="jp-op-table-card-list-item">Life Coaching <a href="javascript:void(0);"
+																						class="jp-op-table-card-info"
+																						data-toggle="tooltip"
+																						data-placement="top"
+																						data-trigger="trigger"
+																						data-html="true"
+																						data-template="<div class='jp-op-table-card-tooltip tooltip' role='tooltip'><div class='arrow'></div><div class='tooltip-inner'></div></div>'"
+																						title="<?php echo wp_kses_post( $tooltip ); ?>"><span class="fas fa-info-circle"></span></a></li>
 							</ul>
 						</div>
 					</div>
@@ -157,10 +179,83 @@ get_header();
 							<h4 class="jp-op-table-card-title">Medications</h4>
 
 							<ul class="jp-op-table-card-list">
-								<li class="jp-op-table-card-list-item">For everyone <a href="javascript:void(0);" class="jp-op-table-card-info" data-toggle="tooltip" data-placement="top" data-trigger="trigger" data-html="true" title="Tooltip on top"><span class="fas fa-info-circle"></span></a></li>
-								<li class="jp-op-table-card-list-item">For alcohol <a href="javascript:void(0);" class="jp-op-table-card-info" data-toggle="tooltip" data-placement="top" data-trigger="trigger" data-html="true" title="Tooltip on top"><span class="fas fa-info-circle"></span></a></li>
-								<li class="jp-op-table-card-list-item">For opiates <a href="javascript:void(0);" class="jp-op-table-card-info" data-toggle="tooltip" data-placement="top" data-trigger="trigger" data-html="true" title="Tooltip on top"><span class="fas fa-info-circle"></span></a></li>
-								<li class="jp-op-table-card-list-item">For underlying issues <a href="javascript:void(0);" class="jp-op-table-card-info" data-toggle="tooltip" data-placement="top" data-trigger="trigger" data-html="true" title="Tooltip on top"><span class="fas fa-info-circle"></span></a></li>
+								<?php
+									$tooltip = '<b>Naltrexone</b> (Vivitrol, Revia, Depade) is non-addictive. The medication blocks the positive effects of alcohol and drugs to discourage relapse, while also reducing cravings.
+									<br>
+									It can be started day 7-10 of sobriety and has no serious side effects. Because of cost, it\'s usually used for 1-12 months, but could be taken forever.
+									<br>
+									In shot form (Vivitrol), it’s administered once a month at the clinic, eliminating the option to skip or mistake the dose. If not covered by insurance Vivitrol is $1,000 per monthly shot, though they offer a <a href=\'https://www.vivitrol.com/co-pay-savings-program\' target=\'_blank\'>savings card</a>. The other options are Revia and Depade. These daily oral pills are around $100 a month.
+									<br><br>
+									<b>Gabapentin</b> (Neurontin, Gralise, Horizant) is a medication that has been studied recently for cravings. While research is ongoing, the takeaway is that Gabapentin is addictive and rarely the right choice.';
+								?>
+
+								<li class="jp-op-table-card-list-item">For everyone <a href="javascript:void(0);"
+																						class="jp-op-table-card-info"
+																						data-toggle="tooltip"
+																						data-placement="top"
+																						data-trigger="trigger"
+																						data-html="true"
+																						data-template="<div class='jp-op-table-card-tooltip tooltip' role='tooltip'><div class='arrow'></div><div class='tooltip-inner'></div></div>'"
+																						title="<?php echo wp_kses_post( $tooltip ); ?>"><span class="fas fa-info-circle"></span></a></li>
+
+								<?php
+									$tooltip = 'While Naltrexone is usually the first choice, the following medications are also FDA-approved specifically for alcoholism:
+									<br>
+									<ul>
+									<li><b>Acamprosate</b> (Campral) reduces symptoms of long-lasting withdrawal and cravings, such as insomnia, anxiety, restlessness and generally feeling unwell. It must be taken three times a day.</li>
+									<li><b>Disulfiram</b> (Antabuse) interferes with the body’s ability to absorb alcohol. Instead of feeling “good”, alcohol will cause unpleasant reactions such as nausea and possibly be dangerous. Those who have shown the ability to stay sober for an extended period with recent relapse are ideal candidates.</li>
+									<li><b>Topiramate</b> (Topamax, Qudexy) is an FDA-approved medication for seizures used “off label” to also reduce alcohol cravings due to initial promising research.</li>
+									</ul>';
+								?>
+
+								<li class="jp-op-table-card-list-item">For alcohol <a href="javascript:void(0);"
+																						class="jp-op-table-card-info"
+																						data-toggle="tooltip"
+																						data-placement="top"
+																						data-trigger="trigger"
+																						data-html="true"
+																						data-template="<div class='jp-op-table-card-tooltip tooltip' role='tooltip'><div class='arrow'></div><div class='tooltip-inner'></div></div>'"
+																						title="<?php echo wp_kses_post( $tooltip ); ?>"><span class="fas fa-info-circle"></span></a></li>
+
+								<?php
+									$tooltip = 'In addition to Naltrexone, these medications are FDA-approved to treat heroin or pain pill addiction:
+									<br>
+									<ul>
+									<li><b>Buprenorphine + Naloxone</b> (Suboxone) helps prevent withdrawal symptoms by replacing the effects of opiates with a lesser effect that does not produce a “high.” This is typically administered for 7-10 days or a longer maintenance program. This medication is addictive but poses a much lower risk.</li>
+									<li><b>Methadone</b> (Dolophine, Methadose) is a more dangerous version of the above medication that is no longer recommended.</li>
+									</ul>';
+								?>
+
+								<li class="jp-op-table-card-list-item">For opiates <a href="javascript:void(0);"
+																						class="jp-op-table-card-info"
+																						data-toggle="tooltip"
+																						data-placement="top"
+																						data-trigger="trigger"
+																						data-html="true"
+																						data-template="<div class='jp-op-table-card-tooltip tooltip' role='tooltip'><div class='arrow'></div><div class='tooltip-inner'></div></div>'"
+																						title="<?php echo wp_kses_post( $tooltip ); ?>"><span class="fas fa-info-circle"></span></a></li>
+
+								<?php
+									$tooltip = 'All other physical and mental health issues (like depression and anxiety) need to be treated – whether they helped fuel the addiction in the first place or came as a result of the addiction.
+									Common mental health prescriptions include:
+									<br>
+									<ul>
+									<li><b>Fluoxetine</b> (Prozac, Sarafem) or <b>Bupropion</b> (Wellbutrin) for depression</li>
+									<li><b>Aripiprazole</b> (Abilify) for mood and bipolar</li>
+									<li><b>Trazodone</b> (Oleptro) or <b>Hydroxyzine</b> (Vistaril, Atarax) for sleep and anxiety</li>
+									<li><b>Paroxetine</b> (Paxil, Pexeva, Brisdelle) or <b>Sertraline</b> (Zoloft) for major depress, PTSD, OCD</li>
+									<li><b>Oxcarbazepine</b> (Trileptal) for mood stabilization</li>
+									</ul>';
+								?>
+
+								<li class="jp-op-table-card-list-item">For underlying issues <a href="javascript:void(0);"
+																						class="jp-op-table-card-info"
+																						data-toggle="tooltip"
+																						data-placement="top"
+																						data-trigger="trigger"
+																						data-html="true"
+																						data-template="<div class='jp-op-table-card-tooltip tooltip' role='tooltip'><div class='arrow'></div><div class='tooltip-inner'></div></div>'"
+																						title="<?php echo wp_kses_post( $tooltip ); ?>"><span class="fas fa-info-circle"></span></a></li>
 							</ul>
 						</div>
 					</div>
